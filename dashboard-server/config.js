fa804d88a6189ce146e49e521f110fb9b8554752
mitrotasios <a.mitrotasios13@gmail.com>
2021-06-03 17:38:44 +0100

    minor fixes

--- dashboard-server/config.js
+++ dashboard-server/config.js
@@ -1,21 +1,9 @@
-// module.exports = {
-//     'secretKey': '12345-67890-09876-54321',
-//     'mongoUrl': 'mongodb+srv://admin:6yhOP6DPHaKWNDk1@cluster0.ck23c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// }
-
-// const dbKeys = {
-//     user: 'me',
-//     host: 'localhost',
-//     database: 'cars',
-//     password: 'password',
-//     port: 5432
-// }
-
 const dbKeys = {
-    user: 'puvkuukt',
-    host: 'postgres://puvkuukt:M2Si94WOw3aJUGpwqy3Uova3oURmYo6t@tai.db.elephantsql.com/puvkuukt',
-    database: 'puvkuukt',
-    password: 'M2Si94WOw3aJUGpwqy3Uova3oURmYo6t',
+    user: 'me',
+    host: 'localhost',
+    database: 'cars',
+    password: 'password',
+    port: 5432
 }
 
 module.exports = dbKeys;
